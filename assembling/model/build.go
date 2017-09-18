52a018d59e05990ca024e9a7e38cdc632c75298a
Zhen Ju <juzhen@huawei.com>
2017-09-18 18:37:27 +0800

    Add the Save method for structure model.Build

--- assembling/model/build.go
+++ assembling/model/build.go
@@ -1,12 +1,32 @@
 package model
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Build struct {
-	Namespace  string `sql:"not null;type:varchar(255)"`
-	Repository string `sql:"not null;type:varchar(255)"`
-	Image      string `sql:"not null;type:varchar(255)"`
-	Tag        string `sql:"not null;type:varchar(255)"`
-	Logs       string `sql:"not null;type:text"`
-	Succeed    bool   `sql:"not null"`
+	Registry  string `sql:"not null;type:varchar(255)"`
+	Namespace string `sql:"not null;type:varchar(255)"`
+	Image     string `sql:"not null;type:varchar(255)"`
+	Tag       string `sql:"not null;type:varchar(255)"`
+	BuildLogs string `sql:"not null;type:text"`
+	PushLogs  string `sql:"not null; type:text"`
+	Succeed   bool   `sql:"not null"`
+}
+
+func (b *Build) Save() error {
+	tx := DB.Begin()
+	bs, _ := json.MarshalIndent(b, "", "  ")
+	fmt.Println("going to save:")
+	fmt.Println(string(bs))
+
+	if err := tx.Debug().Create(&b).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
 }
 
 func (b *Build) TableName() string {
